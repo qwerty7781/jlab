0310336c053ff9ca99cfc2def6619821bbdeed8c
qwerty7781 <153929625+qwerty7781@users.noreply.github.com>
2024-02-12 06:22:50 +0400

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+class GFG {
+	public static boolean isPalindrome(int i, int j,
+									String A)
+	{
+
+		if (i >= j) {
+			return true;
+		}
+		if (A.charAt(i) != A.charAt(j)) {
+			return false;
+		}
+		return isPalindrome(i + 1, j - 1, A);
+	}
+	public static boolean isPalindrome(String A)
+	{
+		return isPalindrome(0, A.length() - 1, A);
+	}
+	public static void main(String[] args)
+	{
+		String A = "geeks";
+		A = A.toLowerCase();
+		boolean str = isPalindrome(A);
+		System.out.println(str);
+	}
+}
